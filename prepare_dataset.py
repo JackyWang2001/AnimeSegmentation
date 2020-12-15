d0cfc6222c1948d335d261a0445ad8bca210b873
jpl <jiw010@ucsd.edu>
2020-12-16 03:19:49 +0800

    update

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,7 +2,14 @@ from utils import *
 
 ROOT = os.path.abspath("/media/jpl/T7/ADE20K")
 TRAINING_PATH = os.path.join(ROOT, "images", "training")
-VALIDATION_PATH = os.path.join(ROOT, "images", "validation")
-TRAINING_DIRS = get_subfolder(TRAINING_PATH)
-VALIDATION_DIRS = get_subfolder(VALIDATION_PATH)
+training_dirs = get_subfolder(TRAINING_PATH)
+training_dirs = concat_subfolder(training_dirs)
+training_raw = get_img_path(training_dirs)
 
+VALIDATION_PATH = os.path.join(ROOT, "images", "validation")
+validation_dirs = get_subfolder(VALIDATION_PATH)
+validation_dirs = concat_subfolder(validation_dirs)
+validation_raw = get_img_path(validation_dirs)
+
+copy_to_new(training_raw, "/media/jpl/T7/_Documents/CartoonGAN/ADE/training")
+copy_to_new(validation_raw, "/media/jpl/T7/_Documents/CartoonGAN/ADE/validation")
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import shutil
 
 
 def get_subfolder(path):
@@ -9,3 +10,38 @@ def get_subfolder(path):
 	:return: a list of all sub-folders in dir
 	"""
 	return sorted(glob.glob(os.path.join(path, "*")))
+
+
+def concat_subfolder(dir_list):
+	"""
+	concat all subdirectories
+	:param dir_list: list of dirs like [training/a, training/b, ...]
+	:return: a list of path point to paths like [training/a/abbey, training/a/airfield, ...]
+	"""
+	results = []
+	for path in dir_list:
+		results += get_subfolder(path)
+	return results
+
+
+def get_img_path(dir_list):
+	"""
+	get all images for style transferring, raw images are .jpg files
+	:param dir_list: a list of path point to paths like [training/a/abbey, training/a/airfield, ...]
+	:return: a dict of paths of raw images, key is image name and value is original directory and path
+	"""
+	raw_images = {}
+	for path in dir_list:
+		for img in glob.glob(os.path.join(path, "*.jpg")):
+			img_name = img.split("/")[-1]
+			raw_images[img_name] = [path, img]
+	return raw_images
+
+
+def copy_to_new(img_dict, destination):
+	"""
+	copy raw images to destination
+	"""
+	for img_name in img_dict:
+		shutil.copy(img_dict[img_name][1], destination)
+		print("copying " + img_name)
